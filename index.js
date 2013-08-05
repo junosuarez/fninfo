463c22b14e9093b9b647f09eb68bc943bd070a6c
jden <jason@denizac.org>
2013-08-05 16:20:55 -0700

    fix support for multiline parameter lists

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ var FN_ARG_SPLIT = /\s*,\s*/;
 function info (fn) {
   var src = fn.toString()
   var out = []
-  
+
   var loc = matches(src, newlines) + 1
   src = src.replace(stripcommentsandspaces, '')
   var sloc = matches(src, signewlines) + 1
 
   var params = src.match(FN_ARGS)
-  params = params && params[1] ? params[1].split(FN_ARG_SPLIT) : [];
-  
+  params = params && params[1] ? params[1].trim().split(FN_ARG_SPLIT) : [];
+
   params.sloc = sloc
   params.loc = loc
 
--- test/test.js
+++ test/test.js
@@ -1,53 +1,82 @@
 var fninfo = require('../index')
 var assert = require('assert')
+var chai = require('chai')
+chai.should()
 
-var a = function (foo, bar, baz) {/*
-  
-  */
+describe('fninfo', function () {
+  it('parses parameter list', function () {
 
-  // comment
-  baz = foo + bar
-  return baz
-}
+    var a = function (foo, bar, baz) {/*
 
-var ainfo = fninfo(a)
+      */
 
-assert.equal(ainfo.length, 3)
-assert.equal(ainfo[0], 'foo')
-assert.equal(ainfo[1], 'bar')
-assert.equal(ainfo[2], 'baz')
-assert.equal(ainfo.loc, 8)
-assert.equal(ainfo.sloc, 4)
+      // comment
+      baz = foo + bar
+      return baz
+    }
 
-var b = function () {
-  return true
-}
+    var ainfo = fninfo(a)
 
-var binfo = fninfo(b)
+    assert.equal(ainfo.length, 3)
+    assert.equal(ainfo[0], 'foo')
+    assert.equal(ainfo[1], 'bar')
+    assert.equal(ainfo[2], 'baz')
+    assert.equal(ainfo.loc, 8)
+    assert.equal(ainfo.sloc, 4)
 
-assert.equal(binfo.length, 0)
-assert.equal(binfo.sloc, 3)
-assert.equal(binfo.loc, 3)
+  })
 
+  it('works with nullary functions', function () {
+    var b = function () {
+      return true
+    }
 
-var c = function (x /* can be anything */) { return !x }
+    var binfo = fninfo(b)
 
-var cinfo = fninfo(c)
+    assert.equal(binfo.length, 0)
+    assert.equal(binfo.sloc, 3)
+    assert.equal(binfo.loc, 3)
 
-assert.equal(cinfo.length, 1)
-assert.equal(cinfo[0], 'x')
-assert.equal(cinfo.sloc, 1)
-assert.equal(cinfo.loc, 1)
+  })
 
+  it('supports comments in parameter list', function () {
 
-// should handle overriden `toString`
-var d = function (a, b, c) {}
-d.toString = function () { return 'foo' }
+    var c = function (x /* can be anything */) { return !x }
 
-var dinfo = fninfo(d)
+    var cinfo = fninfo(c)
 
-assert.equal(dinfo.length, 0)
-assert.equal(dinfo.sloc, 1)
-assert.equal(dinfo.loc, 1)
+    assert.equal(cinfo.length, 1)
+    assert.equal(cinfo[0], 'x')
+    assert.equal(cinfo.sloc, 1)
+    assert.equal(cinfo.loc, 1)
 
-console.log('tests ok')
+  })
+
+  it('should handle overriden `toString`', function () {
+
+    var d = function (a, b, c) {}
+    d.toString = function () { return 'foo' }
+
+    var dinfo = fninfo(d)
+
+    assert.equal(dinfo.length, 0)
+    assert.equal(dinfo.sloc, 1)
+    assert.equal(dinfo.loc, 1)
+
+  })
+
+  it('handles multiline parameter lists', function () {
+    var fn = function (z,
+      a,
+      b,
+      c,
+      d
+    ){}
+    var info = fninfo(fn)
+
+    info.length.should.equal(5)
+    // slice to get array
+    info.slice().should.deep.equal(['z','a','b','c','d'])
+  })
+
+})
